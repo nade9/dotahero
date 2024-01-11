47727ee5701845855b3e6546d924233f7c2d5c26
Ivan Moroz <ivanfrost919@gmail.com>
2024-01-11 22:11:34 +0600

    codestyled

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,14 @@
-import Logo from '../assets/dotahero_transparent.png'
-import {Nav, Navbar, NavDropdown} from "react-bootstrap"
-import heroes from "dotaconstants/build/heroes.json"
+import Logo from '../assets/dotahero_transparent.png';
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import heroes from 'dotaconstants/build/heroes.json';
 
-const cdn = "https://cdn.cloudflare.steamstatic.com"
+const cdn = 'https://cdn.cloudflare.steamstatic.com';
 
 export default function NavigationBar() {
     return (
-        <Navbar className="bg-dark-subtle p-0 rounded-5 my-3 px-3">
-            <Navbar.Brand href="#" className="fw-bold me-0">
-                <img className="w-75" src={Logo} alt="DOTAHERO Logo"/>
+        <Navbar className={'bg-dark-subtle p-0 rounded-5 my-3 px-3'}>
+            <Navbar.Brand href={'#'} className={'fw-bold me-0'}>
+                <img className={'w-75'} src={Logo} alt="DOTAHERO Logo"/>
             </Navbar.Brand>
 
             <Navbar.Collapse id="basic-navbar-nav">
@@ -16,13 +16,13 @@ export default function NavigationBar() {
                     <NavDropdown title="HEROES" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#">
                             <img className="me-2"
-                                 src={cdn + heroes["106"]["icon"]}
+                                 src={cdn + heroes['106']['icon']}
                                  alt="Ember Spirit icon"/>
-                            {heroes["106"]["localized_name"]}
+                            {heroes['106']['localized_name']}
                         </NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-    )
+    );
 }
